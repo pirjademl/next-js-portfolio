0e80e636892c5cfdde804b7ce7cac4deaace4488
magdump <pirjademl7@gmail.com>
2024-05-12 19:30:33 +0530

    refactor: add styles for grid responsiveness

--- @/components/sections/projects/Projectcards.tsx
+++ @/components/sections/projects/Projectcards.tsx
@@ -1,23 +1,34 @@
-import {FC} from 'react'
-import Image from 'next/image';
-import { IProjectSectionData } from '@components/@/lib/data'
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '../../ui/card';
-import CardProject from '../../ui/ProjectCard';
-interface CardSectionProps{
-        title:string,
-        version?:string,
-        data:IProjectSectionData[];
+import { FC } from "react";
+import Image from "next/image";
+import { IProjectSectionData } from "@components/@/lib/data";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../../ui/card";
+import CardProject from "../../ui/ProjectCard";
+interface CardSectionProps {
+  title: string;
+  version?: string;
+  data: IProjectSectionData[];
 }
 
-export const CardSection:FC<CardSectionProps>=({title,version,data})=>{
-            return(
-            <section className="mt-4 " >
-            <h3>{title}</h3>
-            <div className="grid grid-cols-4 gap-3">
-               {data.map((project)=>
-                    <CardProject key={project.id} title={project.title} src={project.src} techStack={project.stack}/>
-                )}
-                </div>
-            </section>
-            )
-}
+export const CardSection: FC<CardSectionProps> = ({ title, version, data }) => {
+  return (
+    <section className="mt-4 ">
+      <h3>{title}</h3>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
+        {data.map((project) => (
+          <CardProject
+            key={project.id}
+            title={project.title}
+            src={project.src}
+            techStack={project.stack}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
 import Cover from "@components/@/components/Cover";
 import Image from "next/image";
 import { CardSection } from "@components/@/components/sections/projects/Projectcards";
-import {TechSection} from "@components/@/components/sections/tech/techSection";
-import { Card, CardContent, CardHeader, CardTitle } from "@components/@/components/ui/card";
+import { TechSection } from "@components/@/components/sections/tech/techSection";
 import { ProjectsData } from "@components/@/lib/data";
 import { Code } from "lucide-react";
 import { OthersSection } from "@components/@/components/sections/others/OthersSection";
 export default function Home() {
   return (
-  <main className="flex flex-col">
-  <Cover/>
-  <CardSection title="Projects"  data={ProjectsData}/>
-  <TechSection title="What I  know "/>
-  <OthersSection/>
- </main>
+    <main className="flex flex-col px-3">
+      <Cover />
+      <CardSection title="Projects" data={ProjectsData} />
+      <TechSection title="What I  know " />
+      <OthersSection />
+    </main>
   );
 }
