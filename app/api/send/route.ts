e143a552b6f589d5784c0d7f27e4fba374884bd6
magdump <pirjademl7@gmail.com>
2024-07-09 17:10:24 +0530

    fix:fix query undefined bug while sending mails

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,14 +1,15 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
+import { inputsType } from "../../../@/types/types";
 const resend = new Resend(process.env.NEXT_RESEND_API_KEY);
 
 export async function POST(req: Request) {
-  const { firstName, lastName, email, message } = await req.json();
+  const { firstName, lastName, email, query }: inputsType = await req.json();
   await resend.emails.send({
     from: "Acme <onboarding@resend.dev>",
     to: "pirjademag@gmail.com",
     subject: "contact form query",
-    text: JSON.stringify({ firstName, lastName, email, message }),
+    text: JSON.stringify({ firstName, lastName, email, query }),
   });
 
   return NextResponse.json({
