e1b68b06168ec4091c73ac06c5dc6045120d5e4d
magdump <pirjademl7@gmail.com>
2024-07-09 17:30:07 +0530

    fix:fix email sending endpoint

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -3,12 +3,12 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.NEXT_RESEND_API_KEY);
 
 export async function POST(req: Request) {
-  const { firstName, lastName, email, message } = await req.json();
+  const { firstName, lastName, email, query } = await req.json();
   await resend.emails.send({
     from: "Acme <onboarding@resend.dev>",
     to: "pirjademag@gmail.com",
     subject: "contact form query",
-    text: JSON.stringify({ firstName, lastName, email, message }),
+    text: JSON.stringify({ firstName, lastName, email, query }),
   });
 
   return NextResponse.json({
